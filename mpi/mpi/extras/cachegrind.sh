9f64df24c6c4632c13d8bb95361fbc73bd93b59a
Samuel Coward <sc14809@newblue2.cm.cluster>
2016-11-02 16:00:28 +0000

    removed duplicate folder

--- mpi/mpi/extras/cachegrind.sh
+++
@@ -1,1 +0,0 @@
-valgrind --tool=cachegrind --branch-sim=yes ./d2q9-bgk.exe $1 $2
--- mpi/mpi/extras/obhalf.py
+++
@@ -1,4 +0,0 @@
-f = open('obstacles_half.dat', 'w')
-for y in range(0,128):
-  for x in range(0,256):
-    f.write(str(x) + ' ' + str(y) + ' 1\n')
